2516fd1f1ab3b21520750d8182e7fd6353cce9f3
Martino Caron <martino.caron@gmail.com>
2023-06-01 17:48:19 +0200

    'martino_test_cors_reset'

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,13 +1,11 @@
-const allowedOrigins = ['*']/*[
+const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://palimanager-clo1.it',
     'https://palimanager-clo1.it',
     'http://vps-6e47e4bb.vps.ovh.net',
     'https://vps-6e47e4bb.vps.ovh.net',
     'http://localhost:3000',
-    'http://localhost:8000',
-    '172.31.144.1',
-    '79.60.236.58'
-]*/
+    'http://localhost:8000'
+]
 
 export default allowedOrigins
