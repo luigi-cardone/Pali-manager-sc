a7112b580bac2a895cb2af6fc0366cf81bd85f02
Martino Caron <martino.caron@gmail.com>
2023-06-05 14:22:49 +0200

    'cors_credentials_false'

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,13 +1,12 @@
 const allowedOrigins = [
-    'http://127.0.0.1:3000',
+    /*'http://127.0.0.1:3000',
     'http://palimanager-clo1.it',
     'https://palimanager-clo1.it',
     'http://vps-6e47e4bb.vps.ovh.net',
     'https://vps-6e47e4bb.vps.ovh.net',
     'http://localhost:3000',
-    'http://localhost:8000',
-    '172.19.0.1',
-    '79.60.236.58'
+    'http://localhost:8000'*/
+    '*'
 ]
 
 export default allowedOrigins
--- Server/config/corsOptions.js
+++ Server/config/corsOptions.js
@@ -6,14 +6,7 @@ const corsOptions = {
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-    },
-    //credentials: true, // This MUST be "true" if your endpoint is
-                       // authenticated via either a session cookie
-                       // or Authorization header. Otherwise the
-                       // browser will block the response.
-
-    methods: 'POST,GET,PUT,OPTIONS,DELETE',
-    optionsSuccessStatus: 200
+    }
 }
 
 export default corsOptions
