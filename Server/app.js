821a224bd7feaa7202ebd31538ddd5444bee784d
Martino Caron <martino.caron@gmail.com>
2023-06-12 14:53:19 +0200

    'test-hello'

--- Server/app.js
+++ Server/app.js
@@ -34,25 +34,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express()
-// Add headers before the routes are defined
-/*app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  //res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});*/
 app.use(express.json())
 app.use(credentials)
 app.use(cors(corsOptions))
@@ -79,13 +60,14 @@ app.use('/pl', pl)
 app.use('/pls', pls)
 app.use('/fornitori', fornitori)
 app.use('/fornitore', fornitore)
-/*app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'), function(err) {
+app.get('/*', function(req, res) {
+    res.send("<h1>Hello world </h1>") 
+    /*res.sendFile(path.join(__dirname, '/public/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
-    })
-  })*/
+    })*/
+  })
 
 app.listen(process.env.PORT || 8000, ()=>{
   console.log("Backend started")
